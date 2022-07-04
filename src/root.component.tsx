a60c1739e09649f35ef6f21a935632c69ae28bd8
carol_freitas <caroline@smarketsolutions.com.br>
2022-07-04 08:58:23 -0300

    feat: create microfrontend react-app

---
+++ src/root.component.tsx
@@ -0,0 +1,13 @@
+import "./style.css";
+
+export default function Root(props) {
+  return (
+    <section>
+      <h1>Olá React</h1>
+      <img
+        src='https://logospng.org/download/react/logo-react-512.png'
+        alt=''
+      />
+    </section>
+  );
+}
